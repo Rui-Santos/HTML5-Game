6e00d9e2f01d87e7af921249bd675f26bbd22f73
James Costian <james@jamescostian.com>
2012-03-28 18:01:27 -0500

    Removed stuff

--- js/app/1game.js
+++
@@ -1,8 +0,0 @@
-    var game={
-        leftkey:function(){console.log('Fired: Left key');},
-        upkey:function(){console.log('Fired: Up key');},
-        rightkey:function(){console.log('Fired: Right key');},
-        downkey:function(){console.log('Fired: Down key');},
-        spacekey:function(){console.log('Fired: Space key/bar');},
-        spacebar:function(){game.spacekey();}// Just an alias to the spacekey function.
-    };
